41be4ec1835db8c2d163c85dd1772536d84b01c4
Ilyas2002 <57584228+Ilyas2002@users.noreply.github.com>
2021-06-03 05:57:55 +0500

    Add files via upload

---
+++ Lopushok/Classes/Connect_BD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lopushok.Classes
+{
+    class Connect_BD
+    {
+      //  public static string String { get; } = "Data Source = WSR; Initial Catalog = user9; uid = user9 ; password = passuser9 ;";
+        public static string String { get; } = "Data Source = DESKTOP-SHSPEI8; Initial Catalog = Paper; Integrated Security= true;";
+    }
+}
---
+++ Lopushok/Windows/Edit_MinCount.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lopushok.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Edit_MinCount.xaml
+    /// </summary>
+    public partial class Edit_MinCount : Window
+    {
+        public Edit_MinCount()
+        {
+            InitializeComponent();
+        }
+    }
+}
